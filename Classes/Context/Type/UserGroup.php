32ede74b170c22c572884372ef0cb54f693c8929
Andre Hähnel <andre.haehnel@netresearch.de>
2013-04-05 14:50:27 +0200

    TYPOC-20  add functionality

--- Classes/Context/Type/UserGroup.php
+++ Classes/Context/Type/UserGroup.php
@@ -7,10 +7,38 @@ class Tx_Contexts_Context_Type_UserGroup extends Tx_Contexts_Context_Abstract
 {
     public function match(array $arDependencies = array())
     {
-      
+        $strAllowedUserGroup = $this->getConfValue('field_usergroup');
+        
+        if ($strAllowedUserGroup) {
+           $arCurrentFeGroupIds 
+               = $this->getCurrentFeGroupUids();
+           
+           $arAllowedFeUserGroup = explode(',', $strAllowedUserGroup);
+           
+           foreach ($arAllowedFeUserGroup as $allowedGroup) {
+               if (in_array($allowedGroup, $arCurrentFeGroupIds)) {
+                   return true;
+               }
+           }
+           
+        }
+       
+        return false;
+        
 
-       return false;
-
+    }
+    
+    /**
+     * Get group uids from current fe user
+     * 
+     * @return array group uids from current fe user
+     */
+    protected function getCurrentFeGroupUids()
+    {
+        //NO group data available because the fe user is not initialized
+        global $TSFE;
+        $arData = $TSFE->fe_user->groupData;
+        return $arData['uid'];
     }
 
 }
