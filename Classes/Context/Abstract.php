ae594497b30a7a1a50304dd59720b7314994ec13
Christian Weiske <christian.weiske@netresearch.de>
2013-04-08 09:26:34 +0200

    TYPOC-15: CS

--- Classes/Context/Abstract.php
+++ Classes/Context/Abstract.php
@@ -25,7 +25,10 @@
 /**
  * Abstract context - must be extended by the context types
  *
- * @author Christian Opitz <christian.opitz@netresearch.de>
+ * @package    Contexts
+ * @subpackage Contexts_Type
+ * @author     Christian Opitz <christian.opitz@netresearch.de>
+ * @license    http://opensource.org/licenses/gpl-license GPLv2 or later
  */
 abstract class Tx_Contexts_Context_Abstract
 {
@@ -60,6 +63,7 @@ abstract class Tx_Contexts_Context_Abstract
      * Constructor - set the values from database row
      *
      * @param array $arRow Database context row
+     *
      * @return void
      */
     public function __construct($arRow = array())
@@ -85,6 +89,7 @@ abstract class Tx_Contexts_Context_Abstract
      * @param string $sheet     Sheet pointer, eg. "sDEF
      * @param string $lang      Language pointer, eg. "lDEF
      * @param string $value     Value pointer, eg. "vDEF
+     *
      * @return string The content
      */
     protected function getConfValue(
@@ -108,24 +113,30 @@ abstract class Tx_Contexts_Context_Abstract
      * Query a setting record and retrieve the value object
      * if one was found
      *
-     * @param string $table
-     * @param string $setting
-     * @param string $uid
+     * @param string $table   Database table name
+     * @param string $setting Setting name
+     * @param string $uid     Record UID
+     *
      * @return Tx_Contexts_Context_Setting|null
      */
-    final public function getSetting($table, $setting, $uid) {
+    final public function getSetting($table, $setting, $uid)
+    {
         $settings = $this->getSettings($table, $uid);
-        return array_key_exists($setting, $settings) ? $settings[$setting] : null;
+        return array_key_exists($setting, $settings)
+            ? $settings[$setting]
+            : null;
     }
 
     /**
      * Get all settings of one record
      *
-     * @param string $table
-     * @param int    $uid
+     * @param string $table Database table
+     * @param int    $uid   Record UID
+     *
      * @return array
      */
-    final public function getSettings($table, $uid) {
+    final public function getSettings($table, $uid)
+    {
         $settingsKey = $table . '.' . $uid;
 
         if (array_key_exists($settingsKey, $this->settings)) {
@@ -139,15 +150,16 @@ abstract class Tx_Contexts_Context_Abstract
         $where = 'context_uid = ' . $this->uid;
         $where .= " AND foreign_table = '$table'";
         $where .= " AND foreign_uid IN ('" . implode("','", $uids) . "')";
-        $rows = (array) Tx_Contexts_Api_Configuration::getDb()->exec_SELECTgetRows('*', 'tx_contexts_settings', $where);
+        $rows = (array) Tx_Contexts_Api_Configuration::getDb()
+            ->exec_SELECTgetRows('*', 'tx_contexts_settings', $where);
 
         foreach ($uids as $uid) {
             $this->settings[$table . '.' . $uid] = array();
         }
 
         foreach ($rows as $row) {
-            $this->settings[$table . '.' . $row['foreign_uid']][$row['name']] =
-                new Tx_Contexts_Context_Setting($this, $row);
+            $this->settings[$table . '.' . $row['foreign_uid']][$row['name']]
+                = new Tx_Contexts_Context_Setting($this, $row);
         }
 
         return $this->settings[$settingsKey];
@@ -156,12 +168,14 @@ abstract class Tx_Contexts_Context_Abstract
     /**
      * Determines whether a setting exists for this record
      *
-     * @param string $table
-     * @param string $setting
-     * @param int    $uid
+     * @param string $table   Database table
+     * @param string $setting Setting name
+     * @param int    $uid     Record UID
+     *
      * @return boolean
      */
-    final public function hasSetting($table, $setting, $uid) {
+    final public function hasSetting($table, $setting, $uid)
+    {
         return $this->getSetting($table, $setting, $uid) ? true : false;
     }
 
