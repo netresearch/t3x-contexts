7d56c2ff57bb907b1a938d128750a9202f414a7f
Marian Pollzien <marian.pollzien@netresearch.de>
2013-05-23 13:21:39 +0200

    TYPOC-21: added a check for the combinations showing non existant aliases

---
+++ Classes/Backend.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Part of context extension
+ *
+ * PHP version 5
+ *
+ * @category   TYPO3-Extensions
+ * @package    Contexts
+ * @author     Marian Pollzien <marian.pollzien@netresearch.de>
+ * @license    http://opensource.org/licenses/gpl-license GPLv2 or later
+ * @link       http://github.com/netresearch/contexts
+ */
+
+/**
+ * Provides methods used in the backend by flexforms.
+ *
+ * @category   TYPO3-Extensions
+ * @package    Contexts
+ * @author     Marian Pollzien <marian.pollzien@netresearch.de>
+ * @license    http://opensource.org/licenses/gpl-license GPLv2 or later
+ * @link       http://github.com/netresearch/contexts
+ */
+class Tx_Contexts_Backend
+{
+    /**
+     * Display a textarea with validation for the entered aliases
+     *
+     * @param array          $arFieldInfo Information about the current input field
+     * @param t3lib_tceforms $tceforms    Form rendering library object
+     * @return string HTML code
+     */
+    public function textCombinations($arFieldInfo, t3lib_tceforms $tceforms)
+    {
+        $text = $tceforms->getSingleField_typeText(
+            $arFieldInfo['table'], $arFieldInfo['field'],
+            $arFieldInfo['row'], $arFieldInfo
+        );
+        $evaluator = new Tx_Contexts_Context_Type_Combination_LogicalExpressionEvaluator();
+        $arTokens = $evaluator->tokenize($arFieldInfo['itemFormElValue']);
+
+        $arNotFound = array();
+        foreach ($arTokens as $token) {
+            if (is_array($token)
+                && $token[0] === Tx_Contexts_Context_Type_Combination_LogicalExpressionEvaluator::T_VAR
+            ) {
+                $contexts = Tx_Contexts_Context_Container::get()->initAll();
+                $bFound = false;
+                foreach ($contexts as $context) {
+                    if ($context->getAlias() == $token[1]) {
+                        $bFound = true;
+                    }
+                }
+
+                if (!$bFound) {
+                    $arNotFound[] = $token[1];
+                }
+            }
+        }
+
+        if (!$arNotFound) {
+            return $text;
+        }
+
+        $strNotFound = implode(', ', $arNotFound);
+        $html = <<<HTM
+$text<br />
+{$GLOBALS['LANG']->sL('LLL:EXT:contexts/Resources/Private/Language'
+    .'/flexform.xml:aliasesNotFound')}: $strNotFound
+HTM;
+
+        return $html;
+    }
+}
+?>
