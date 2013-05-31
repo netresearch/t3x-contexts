0771b382bbf6ff8fbc91a0484905399b5d232ebe
Rico Sonntag <rico.sonntag@netresearch.de>
2013-05-31 13:42:28 +0200

    TYPOC-48: Fix missing commata in SQL

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,50 +2,56 @@
 # Table structure for table 'tx_contexts_contexts'
 #
 CREATE TABLE tx_contexts_contexts (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	type tinytext,
-	title tinytext,
-	alias tinytext,
-	type_conf mediumtext,
-	invert tinyint(4) DEFAULT '0' NOT NULL,
-	use_session tinyint(4) DEFAULT '0' NOT NULL,
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+    tstamp int(11) DEFAULT '0' NOT NULL,
+    crdate int(11) DEFAULT '0' NOT NULL,
+    cruser_id int(11) DEFAULT '0' NOT NULL,
+    deleted tinyint(4) DEFAULT '0' NOT NULL,
+    type tinytext,
+    title tinytext,
+    alias tinytext,
+    type_conf mediumtext,
+    invert tinyint(4) DEFAULT '0' NOT NULL,
+    use_session tinyint(4) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+    PRIMARY KEY (uid),
+    KEY parent (pid)
 ) ENGINE=InnoDB;
 
 #
 # Table structure for table 'tx_contexts_settings'
 #
 CREATE TABLE tx_contexts_settings (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	context_uid int(11) DEFAULT '0' NOT NULL,
-	foreign_table tinytext,
-	foreign_uid int(11) DEFAULT '0' NOT NULL,
-	name tinytext,
-	enabled tinyint(4) DEFAULT '0' NOT NULL,
+    uid int(11) NOT NULL auto_increment,
+    pid int(11) DEFAULT '0' NOT NULL,
+    tstamp int(11) DEFAULT '0' NOT NULL,
+    crdate int(11) DEFAULT '0' NOT NULL,
+    cruser_id int(11) DEFAULT '0' NOT NULL,
+    context_uid int(11) DEFAULT '0' NOT NULL,
+    foreign_table tinytext,
+    foreign_uid int(11) DEFAULT '0' NOT NULL,
+    name tinytext,
+    enabled tinyint(4) DEFAULT '0' NOT NULL,
 
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+    PRIMARY KEY (uid),
+    KEY parent (pid)
 ) ENGINE=InnoDB;
 
+#
+# Update structure of table 'pages'
+#
 CREATE TABLE pages (
-       tx_contexts_enable tinytext NOT NULL,
-       tx_contexts_disable tinytext NOT NULL
-       tx_contexts_nav_enable tinytext NOT NULL,
-       tx_contexts_nav_disable tinytext NOT NULL
+    tx_contexts_enable tinytext NOT NULL,
+    tx_contexts_disable tinytext NOT NULL,
+    tx_contexts_nav_enable tinytext NOT NULL,
+    tx_contexts_nav_disable tinytext NOT NULL
 );
 
+#
+# Update structure of table 'tt_content'
+#
 CREATE TABLE tt_content (
-       tx_contexts_enable tinytext NOT NULL,
-       tx_contexts_disable tinytext NOT NULL
+    tx_contexts_enable tinytext NOT NULL,
+    tx_contexts_disable tinytext NOT NULL
 );
