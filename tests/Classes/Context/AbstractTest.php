1ff1205b5ba48bcf7e7738535d3743532a536710
Christian Weiske <christian.weiske@netresearch.de>
2013-05-31 17:25:42 +0200

    TYPOC-53: make abstract test use testbase

--- tests/Classes/Context/AbstractTest.php
+++ tests/Classes/Context/AbstractTest.php
@@ -1,7 +1,8 @@
 <?php
-require_once __DIR__ . '../../../../Classes/Context/Abstract.php';
+require_once __DIR__ . '/../../TestBase.php';
+require_once __DIR__ . '/../../../Classes/Context/Abstract.php';
 
-require_once __DIR__ . '../../../../../../../t3lib/class.t3lib_div.php';
+require_once __DIR__ . '/../../../../../../t3lib/class.t3lib_div.php';
 
 if (!class_exists('t3lib_div')
     && class_exists('TYPO3\CMS\Core\Utility\GeneralUtility')
@@ -11,7 +12,7 @@ if (!class_exists('t3lib_div')
     }
 }
 
-class Tx_Contexts_Context_AbstractTest extends PHPUnit_Framework_TestCase
+class Tx_Contexts_Context_AbstractTest extends TestBase
 {
     /**
      * @dataProvider SessionProvider
@@ -55,50 +56,13 @@ class Tx_Contexts_Context_AbstractTest extends PHPUnit_Framework_TestCase
             array('1','whatever', array(true,true)),
         );
     }
-
-    /**
-     * Make a proteced/private method accessible.
-     *
-     * @param string $strClass      Class name
-     * @param string $strMethodName Method name
-     *
-     * @return ReflectionMethod
-     */
-    public function getAccessibleMethod($strClass, $strMethodName)
-    {
-        $reflectedClass = new ReflectionClass($strClass);
-
-        /* @var $method ReflectionMethod */
-        $method = $reflectedClass->getMethod($strMethodName);
-        $method->setAccessible(true);
-
-        return $method;
-    }
-
-    /**
-     * Call a protected method on an object and return the result
-     *
-     * @param object $obj           Object the method should be called on
-     * @param string $strMethodName Method to be called
-     * @param mixed  $param1        First method parameter
-     * @param mixed  $param2        Second method parameter
-     * @param mixed  ...            ...
-     *
-     * @return mixed Whatever the method returns
-     */
-    public function callProtected($obj, $strMethodName)
-    {
-        $params = func_get_args();
-        array_shift($params);
-        array_shift($params);
-        $m = $this->getAccessibleMethod(get_class($obj), $strMethodName);
-        return $m->invokeArgs($obj, $params);
-    }
 }
 
 class test extends Tx_Contexts_Context_Abstract
 {
-    public function match(array $arDependencies = array()){}
+    public function match(array $arDependencies = array())
+    {
+    }
 }
 
 ?>
