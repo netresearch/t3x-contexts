1e32fd1aa3acf6a3ad40de5f6eaf0bc5093c31d5
Christian Weiske <christian.weiske@netresearch.de>
2013-06-03 11:25:46 +0200

    TYPOC-58: release version 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "contexts".
 #
-# Auto generated 30-01-2013 17:06
+# Auto generated 03-06-2013 11:37
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -14,8 +14,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'title' => 'TYPO3 contexts',
 	'description' => 'Contexts for TYPO3 - can be used to modify page behaviour in the frontend based on several conditions',
 	'category' => 'misc',
-	'author' => 'Christian Opitz',
-	'author_email' => 'christian.opitz@netresearch.de',
+	'author' => 'Andre Hähnel, Christian Opitz, Christian Weiske, Marian Pollzien, Rico Sonntag',
+	'author_email' => 'typo3.org@netresearch.de',
 	'shy' => '',
 	'dependencies' => '',
 	'conflicts' => '',
@@ -26,19 +26,25 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => 0,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => true,
+	'clearCacheOnLoad' => 1,
 	'lockType' => '',
-	'author_company' => '',
-	'version' => '0.0.0',
+	'author_company' => 'Netresearch GmbH & Co.KG',
+	'version' => '0.2.0',
 	'constraints' => array(
 		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
+			'contexts_geolocation' => '',
+			'contexts_wurfl' => '',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"b7ba";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"7804";s:14:"ext_tables.sql";s:4:"a048";s:29:"icon_tx_contexts_contexts.gif";s:4:"475a";s:16:"Resources/Private/Language/locallang_db.xml";s:4:"5571";s:7:"tca.php";s:4:"5233";s:19:"doc/wizard_form.dat";s:4:"6f9c";s:20:"doc/wizard_form.html";s:4:"7f6d";}',
+	'_md5_values_when_last_written' => 'a:46:{s:9:"ChangeLog";s:4:"4372";s:16:"ext_autoload.php";s:4:"8e8a";s:16:"ext_contexts.php";s:4:"098d";s:12:"ext_icon.gif";s:4:"d6c7";s:17:"ext_localconf.php";s:4:"ca59";s:14:"ext_tables.php";s:4:"6cd8";s:14:"ext_tables.sql";s:4:"c85c";s:29:"icon_tx_contexts_contexts.gif";s:4:"d6c7";s:10:"README.rst";s:4:"7301";s:7:"tca.php";s:4:"eb80";s:19:"Classes/Backend.php";s:4:"b19a";s:21:"Classes/Exception.php";s:4:"b002";s:29:"Classes/Api/Configuration.php";s:4:"b064";s:22:"Classes/Api/Record.php";s:4:"6327";s:28:"Classes/Context/Abstract.php";s:4:"9f89";s:29:"Classes/Context/Container.php";s:4:"2c0d";s:27:"Classes/Context/Factory.php";s:4:"14cb";s:27:"Classes/Context/Setting.php";s:4:"cee6";s:36:"Classes/Context/Type/Combination.php";s:4:"3468";s:32:"Classes/Context/Type/Default.php";s:4:"2541";s:31:"Classes/Context/Type/Domain.php";s:4:"4432";s:33:"Classes/Context/Type/GetParam.php";s:4:"fd3f";s:27:"Classes/Context/Type/Ip.php";s:4:"149c";s:63:"Classes/Context/Type/Combination/LogicalExpressionEvaluator.php";s:4:"a962";s:73:"Classes/Context/Type/Combination/LogicalExpressionEvaluator/Exception.php";s:4:"582b";s:45:"Classes/Context/Type/GetParam/TsfeService.php";s:4:"40c2";s:27:"Classes/Service/Install.php";s:4:"957e";s:24:"Classes/Service/Page.php";s:4:"9bb3";s:23:"Classes/Service/Tca.php";s:4:"bd91";s:27:"Classes/Service/Tcemain.php";s:4:"82d6";s:24:"Classes/Service/Tsfe.php";s:4:"ee74";s:50:"Configuration/flexform/ContextType/Combination.xml";s:4:"a821";s:45:"Configuration/flexform/ContextType/Domain.xml";s:4:"9b14";s:47:"Configuration/flexform/ContextType/GetParam.xml";s:4:"40b6";s:41:"Configuration/flexform/ContextType/Ip.xml";s:4:"0d38";s:39:"Resources/Private/Language/flexform.xml";s:4:"f066";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"ad40";s:37:"Resources/Private/csh/Combination.xml";s:4:"0601";s:34:"Resources/Public/StyleSheet/be.css";s:4:"1db5";s:17:"tests/phpunit.xml";s:4:"368c";s:18:"tests/TestBase.php";s:4:"cc35";s:38:"tests/Classes/Context/AbstractTest.php";s:4:"e3e6";s:46:"tests/Classes/Context/Type/CombinationTest.php";s:4:"3103";s:43:"tests/Classes/Context/Type/GetParamTest.php";s:4:"78ce";s:37:"tests/Classes/Context/Type/IpTest.php";s:4:"4194";s:73:"tests/Classes/Context/Type/Combination/LogicalExpressionEvaluatorTest.php";s:4:"b508";}',
+	'comment' => 'First public version',
+	'suggests' => array(
+	),
 );
 
 ?>
