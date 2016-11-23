e105169b0e2381ff7cb126fb1cdfacdd4254c94d
André Hähnel <andre.haehnel@netresearch.de>
2016-11-23 15:31:31 +0100

    fix enableContextsForTable

--- ext_localconf.php
+++ ext_localconf.php
@@ -44,6 +44,9 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_chec
 // register context classes
 require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('contexts', 'Configuration/TCA/Overrides/tx_contexts_contexts.php');
 
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('contexts', 'Configuration/TCA/Overrides/pages.php');
+require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('contexts', 'Configuration/TCA/Overrides/tt_content.php');
+
 // load the custom typoscript condition here
 if (TYPO3_MODE == 'FE') {
     require_once \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('contexts', 'Resources/Private/PHP/TypoScriptConditionMatcher.php');
--- ext_tables.php
+++ ext_tables.php
@@ -7,15 +7,15 @@ defined('TYPO3_MODE') || die();
 );
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_contexts_contexts');
 
-//TODO
-//$icons = array(
-//    'status-overlay-contexts' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'overlay-contexts.png',
-//);
-//\TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons($icons, 'contexts');
-//$GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayPriorities'][150] = 'contexts';
-//$GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayNames']['contexts']
-//    = 'extensions-contexts-status-overlay-contexts';
-
+if (class_exists('TYPO3\CMS\Backend\Sprite\SpriteManager')) {
+    $icons = array(
+        'status-overlay-contexts' => \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extRelPath($_EXTKEY) . 'Resources/Public/Icons/overlay-contexts.png',
+    );
+    \TYPO3\CMS\Backend\Sprite\SpriteManager::addSingleIcons($icons, 'contexts');
+    $GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayPriorities'][150] = 'contexts';
+    $GLOBALS['TBE_STYLES']['spriteIconApi']['spriteIconRecordOverlayNames']['contexts']
+        = 'extensions-contexts-status-overlay-contexts';
+}
 
 $GLOBALS['TBE_STYLES']['skins']['contexts'] = array (
     'name' => 'contexts',
