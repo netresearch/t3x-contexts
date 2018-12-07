a0604b07afc86ebb2d96a847ba77d3ea1e846697
Achim Fritz <achim.fritz@b13.de>
2018-12-07 13:31:00 +0100

    [CLEANUP] rm debug stuff

--- Classes/Query/Restriction/ContextRestriction.php
+++ Classes/Query/Restriction/ContextRestriction.php
@@ -1,16 +1,43 @@
 <?php
 namespace Netresearch\Contexts\Query\Restriction;
 
+/***************************************************************
+ *  Copyright notice - MIT License (MIT)
+ *
+ *  (c) 2018 b:dreizehn GmbH,
+ * 		Achim Fritz <achim.fritz@b13.de>
+ *  All rights reserved
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ ***************************************************************/
+
 use TYPO3\CMS\Core\Database\Query\Restriction\EnforceableQueryRestrictionInterface;
 use TYPO3\CMS\Core\Database\Query\Restriction\QueryRestrictionInterface;
 use TYPO3\CMS\Core\Database\Query\Expression\CompositeExpression;
 use TYPO3\CMS\Core\Database\Query\Expression\ExpressionBuilder;
-
 use Netresearch\Contexts\Api\Configuration;
 use Netresearch\Contexts\Context\Container;
-use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 
-
+/**
+ * Class ContextRestriction
+ * @package Netresearch\Contexts\Query\Restrictio
+ */
 class ContextRestriction implements QueryRestrictionInterface, EnforceableQueryRestrictionInterface
 {
     /**
@@ -28,9 +55,7 @@ class ContextRestriction implements QueryRestrictionInterface, EnforceableQueryR
      */
     public function buildExpression(array $queriedTables, ExpressionBuilder $expressionBuilder): CompositeExpression
     {
-
         $constraints = [];
-
         if ($this->isEnvironmentInFrontendMode()) {
             foreach ($queriedTables as $table) {
                 foreach (Configuration::getEnableSettings($table) as $setting) {
@@ -38,7 +63,6 @@ class ContextRestriction implements QueryRestrictionInterface, EnforceableQueryR
                     if (!$flatColumns) {
                         continue;
                     }
-
                     $enableConstraints = [
                         $expressionBuilder->isNull($flatColumns[1]),
                         $expressionBuilder->eq(
@@ -46,26 +70,20 @@ class ContextRestriction implements QueryRestrictionInterface, EnforceableQueryR
                             $expressionBuilder->literal('')
                         )
                     ];
-
                     $disableConstraints = [];
-
                     foreach (Container::get() as $context) {
-
                         $enableConstraints[] = $expressionBuilder->inSet(
                             $flatColumns[1],
                             (int)$context->getUid()
                         );
-
                         $disableConstraints[] = 'NOT ' . $expressionBuilder->inSet(
                                 $flatColumns[0],
                                 (int)$context->getUid()
                             );
                     }
-
                     $constraints[] = $expressionBuilder->orX(
                         ...$enableConstraints
                     );
-
                     if (count($disableConstraints)) {
                         $constraints[] = $expressionBuilder->orX(
                             $expressionBuilder->isNull($flatColumns[0]),
@@ -76,13 +94,9 @@ class ContextRestriction implements QueryRestrictionInterface, EnforceableQueryR
                             $expressionBuilder->andX(...$disableConstraints)
                         );
                     }
-                    #DebuggerUtility::var_dump($table);
-                    #DebuggerUtility::var_dump($constraints);
-                    #throw new \Exception('foo', 1);
                 }
             }
         }
-
         return $expressionBuilder->andX(...$constraints);
     }
 
