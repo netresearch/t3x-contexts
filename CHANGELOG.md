696f5371faefb0614f98e655e094932b12f4a056
Sebastian Koschel <sebastian.koschel@netresearch.de>
2023-02-02 11:48:40 +0100

    [RELEASE] v3.0.0

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# 3.0.0
+
+## FEATURE
+
+- [FEATURE] Rework code for TYPO v11 compatibility
+
+
+## Contributors
+
+- Achim Fritz
+- Andre Hähnel
+- Axel Kummer
+- Benni Mack
+- Christian Opitz
+- Christian Weiske
+- David Steeb
+- Marian Pollzien
+- Mathias Brodala
+- Michael Schams
+- Rico Sonntag
+- Sebastian Mendel
+- Sebastian Mendel
+- Thomas Schöne
+- Tobias Hein
+- copitz
+
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,5 @@
 <?php
 
-/**
- * This file is part of the package netresearch/contexts.
- *
- * For the full copyright and license information, please read the
- * LICENSE file that was distributed with this source code.
- */
-
-declare(strict_types=1);
-
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Multi-channel contexts',
     'description' => 'Contexts for TYPO3 - can be used to modify page behaviour in the frontend based on several conditions',
@@ -19,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author_company' => 'Netresearch GmbH & Co. KG',
-    'version' => '2.0.0',
+    'version' => '3.0.0',
     'constraints' => [
         'depends' => [
             'typo3' => '11.0.0-11.5.99',
