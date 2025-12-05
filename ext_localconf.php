61dea44b583a19d70acb690430ef6a9e3fae9faa
Sebastian Mendel <info@sebastianmendel.de>
2025-12-05 19:39:50 +0100

    Update composer.json and extension metadata for TYPO3 v12/v13
    - Update PHP requirement to ^8.2
    - Add TYPO3 v12.4 and v13.4 LTS support
    - Update extension metadata and dependencies

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,85 +13,47 @@ use Netresearch\Contexts\Form\CombinationFormElement;
 use Netresearch\Contexts\Form\DefaultSettingsFormElement;
 use Netresearch\Contexts\Form\RecordSettingsFormElement;
 use Netresearch\Contexts\Query\Restriction\ContextRestriction;
-use Netresearch\Contexts\Service\DataHandlerService;
-use Netresearch\Contexts\Service\IconService;
-use Netresearch\Contexts\Service\PageService;
 use Netresearch\Contexts\Xclass\Backend\Tree\Repository\PageTreeRepository;
-use TYPO3\CMS\Core\Imaging\IconFactory;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Object\Container\Container;
 
 defined('TYPO3') || die('Access denied.');
 
-
+// Add context fields to rootline
+// TYPO3 v13: addRootLineFields may not be initialized by default
+if (!isset($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] = '';
+}
 $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= ',tx_contexts_enable,tx_contexts_disable';
 
-
 // XClass: This is needed for the context fields in the page tree
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository::class] = [
-    'className' => PageTreeRepository::class
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository::class] = [
+    'className' => PageTreeRepository::class,
 ];
 
-// Register extended classes in dependency injection container (TYPO3 9.5 - 11.5, not required in TYPO3 12)
-GeneralUtility::makeInstance(Container::class)
-    ->registerImplementation(
-        \TYPO3\CMS\Backend\Tree\Repository\PageTreeRepository::class,
-        PageTreeRepository::class
-    );
-
-
-// Hook into record saving
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][]
-    = DataHandlerService::class;
-
-// Contexts query restriction
+// Contexts query restriction - applied to all database queries
 if (!isset($GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][ContextRestriction::class])) {
     $GLOBALS['TYPO3_CONF_VARS']['DB']['additionalQueryRestrictions'][ContextRestriction::class] = [];
 }
 
-
-// Override page menu visibility
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/tslib/class.tslib_menu.php']['filterMenuPages'][]
-    = PageService::class;
-
-// Override page hash generation
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['createHashBase'][]
-    = 'Netresearch\Contexts\Service\PageService->createHashBase';
-
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS'][IconFactory::class]['overrideIconOverlay'][]
-    = IconService::class;
-
-
-// Register custom form elements
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][12345] = [
+// Register custom form elements for TCA
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1700000001] = [
     'nodeName' => 'defaultSettingsFormElement',
     'priority' => 40,
-    'class'    => DefaultSettingsFormElement::class,
+    'class' => DefaultSettingsFormElement::class,
 ];
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][12346] = [
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1700000002] = [
     'nodeName' => 'combinationFormElement',
     'priority' => 40,
-    'class'    => CombinationFormElement::class,
+    'class' => CombinationFormElement::class,
 ];
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][12347] = [
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1700000003] = [
     'nodeName' => 'recordSettingsFormElement',
     'priority' => 40,
-    'class'    => RecordSettingsFormElement::class,
+    'class' => RecordSettingsFormElement::class,
 ];
 
-
-// Add some hooks
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_checkEnableFields']['contexts']
-    = 'Netresearch\Contexts\Service\FrontendControllerService->checkEnableFields';
-
-
-// Signal is no longer available since TYPO3 v10, Rework somehow if really required or remove stuff
-//$signalSlotDispatcher = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\SignalSlot\Dispatcher::class);
-//$signalSlotDispatcher->connect(
-//    \TYPO3\CMS\Extensionmanager\Utility\InstallUtility::class,
-//    'tablesDefinitionIsBeingBuilt',
-//    \Netresearch\Contexts\Service\InstallService::class,
-//    'appendTableDefinitions'
-//);
+// DataHandler hooks for processing context settings during record save/update
+// Note: TYPO3 v12/v13 DataHandler still uses SC_OPTIONS hooks, not PSR-14 events
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['contexts']
+    = Netresearch\Contexts\Service\DataHandlerService::class;
