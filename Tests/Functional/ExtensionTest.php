6c155c256e305f39afc167669bce256b30fd4e6f
Sebastian Mendel <info@sebastianmendel.de>
2025-12-05 19:44:00 +0100

    Update functional tests for TYPO3 v12/v13
    - Modernize functional test setup
    - Add test fixtures for pages, contexts, settings
    - Remove obsolete bootstrap.php (using TYPO3 testing framework)

--- Tests/Functional/ExtensionTest.php
+++ Tests/Functional/ExtensionTest.php
@@ -7,19 +7,29 @@
  * LICENSE file that was distributed with this source code.
  */
 
+declare(strict_types=1);
+
 namespace Netresearch\Contexts\Tests\Functional;
 
-class ExtensionTest extends \TYPO3\TestingFramework\Core\Functional\FunctionalTestCase
+use PHPUnit\Framework\Attributes\Test;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+/**
+ * Functional test to verify extension is properly loaded.
+ */
+final class ExtensionTest extends FunctionalTestCase
 {
-    protected $testExtensionsToLoad = array('typo3conf/ext/contexts');
+    protected array $testExtensionsToLoad = [
+        'netresearch/contexts',
+    ];
 
-
-    public function testIsExtensionLoaded()
+    #[Test]
+    public function extensionIsLoaded(): void
     {
-        $this->assertTrue(
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded(
-                'contexts'
-            )
+        self::assertTrue(
+            ExtensionManagementUtility::isLoaded('contexts'),
+            'Extension "contexts" should be loaded',
         );
     }
 }
