f06ee054fb865bdcd84d756a034eae4244867c4e
Sebastian Mendel <info@sebastianmendel.de>
2025-12-06 02:20:13 +0100

    build: add Fractor configuration for TYPO3 migrations
    Configure Fractor to process FlexForms, TypoScript, Fluid, and YAML files
    for automated non-PHP migrations during TYPO3 upgrades.

---
+++ fractor.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Fractor configuration for TYPO3 extension upgrade
+ *
+ * Handles non-PHP file migrations:
+ * - FlexForms (XML)
+ * - TypoScript
+ * - YAML (Services.yaml)
+ * - Fluid templates
+ *
+ * @see https://github.com/andreaswolf/fractor
+ */
+
+declare(strict_types=1);
+
+use a9f\Fractor\Configuration\FractorConfiguration;
+use a9f\Typo3Fractor\Set\Typo3LevelSetList;
+
+return FractorConfiguration::configure()
+    ->withPaths([
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Resources',
+    ])
+    ->withSkip([
+        __DIR__ . '/.Build',
+        __DIR__ . '/vendor',
+    ])
+    ->withSets([
+        // TYPO3 v12 migrations for FlexForms, TypoScript, YAML, Fluid
+        // Note: Don't use UP_TO_TYPO3_13 if extension supports both ^12.4 || ^13.4
+        Typo3LevelSetList::UP_TO_TYPO3_12,
+    ]);
